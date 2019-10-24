a8eac43829993c131220d84a7f52fdf0bc1c7b8c
Christian Koestlin <christian.koestlin@esrlabs.com>
2019-10-24 20:27:31 +0200

    Add my fish functions

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
-echo "Hello World"
 if not functions -q fisher
     set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
     curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
     fish -c fisher
 end
+
+rvm ruby-2.6.3@global
